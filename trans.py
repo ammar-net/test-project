5a3031d2bc45d953f251cc45905096b7dd5b8d63
ent <73624550+ammar-net@users.noreply.github.com>
2020-11-02 01:35:54 +0200

    translate
    from googletrans import Translator
    translator = Translator()
    print(translator.translate("مرحبا", dest="ko").text)
    print(translator.translate("مرحبا", dest="ar").text)
    print(translator.translate("مرحبا", dest="ja").text)
    print(translator.translate("مرحبا", dest="it").text)
    print(translator.translate("مرحبا", dest="ru").text)
    print(translator.translate("مرحبا", dest="ur").text)

--- trans.py
+++ trans.py
@@ -5,7 +5,7 @@ print(translator.translate("مرحبا", dest="ar").text)
 print(translator.translate("مرحبا", dest="ja").text)
 print(translator.translate("مرحبا", dest="it").text)
 print(translator.translate("مرحبا", dest="ru").text)
-print(translator.translate("مرحبا", dest="ur").text)
+print(translator.translate("welcom", dest="ur").text)
 
 
 
